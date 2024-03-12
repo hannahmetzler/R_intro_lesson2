64ae509c5bf77ad676f27b9ee6ad54aa4fe7a15a
Hannah Metzler <metzler@csh.ac.at>
2024-03-12 16:51:09 +0100

    looked at data, made boxplot

---
+++ code/rintro_lesson2.R
@@ -0,0 +1,42 @@
+## LOAD PACKAGES ####
+library(tidyverse)
+
+## READ IN DATA AND ORGANIZE ####
+# Read in data
+data = read_csv("data/pets.csv",  col_types = "cffiid")
+
+# Look at data
+dim(data) 
+head(data) 
+tail(data) 
+xtabs(~pet, data) 
+
+dplyr::glimpse(data)
+
+# Subset out cats
+data_cats <- data %>%
+  dplyr::filter(pet == "cat")
+data_cats #print out the new data frame
+
+# Look at cats data
+dim(data_cats) 
+head(data_cats) 
+tail(data_cats) 
+xtabs(~pet, data_cats) 
+xtabs(~country, data_cats)
+
+## MAKE FIGURES ####
+# Weight by pet
+data.plot <- ggplot(data, aes(x = pet, y = weight)) +
+  geom_boxplot()
+data.plot
+
+# Pretty plot
+data.plot <- ggplot(data, aes(x = pet, y = weight, fill = pet)) +
+  geom_boxplot()+
+  theme_bw()+
+  ggtitle("Weight by pet")
+data.plot
+
+# save the plot
+ggsave('figures/boxplot_pets_weight.png', plot = data.plot, width = 6, height = 4, dpi = 300)
